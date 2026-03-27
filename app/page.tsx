118993023eb05f2fe6e10f49da56d2e46e74e1c1
Ayan Ranjan Naskar <ayanranjannaskar2004@gmail.com>
2026-03-27 19:59:06 +0530

    explorebtn

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import ExploreBtn from '@/components/ExploreBtn'
 import React from 'react'
 
 const Page = () => {
   return (
     <section>
-      <h1 className='text-center'>The Hub For Every Dev Event You Mustn't Miss!</h1>
+      <h1 className='text-center'>The Hub For Every Dev <br /> Event You Mustn't Miss!</h1>
+      <p className='text-center mt-4'>Hackathons, Meetups, and Conferences for Developers all in one place</p>
+      <ExploreBtn />
     </section>
   )
 }
