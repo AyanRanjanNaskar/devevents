5da3a3e0bf480ffcb731e73e40569283e47cc0fd
Ayan Ranjan Naskar <ayanranjannaskar2004@gmail.com>
2026-03-28 20:48:54 +0530

    Event Optimizing

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -2,26 +2,68 @@ import Image from "next/image";
 import Link from "next/link";
 
 interface Props {
-    title: string;
-    image: string;
-    slug: string;
-    location: string;
-    date: string;
-    time: string;
-    description: string;
+	title: string;
+	image: string;
+	slug: string;
+	location: string;
+	date: string;
+	time: string;
+	description: string;
 }
 
-const EventCard = ({ title, image, slug, location, date, time, description}: Props) => {
-  return (
-    <Link href={`/events/${slug}`} id="event-card">
-        <Image src={image} alt={title} width={410} height={300} className="poster"/>
-        <p className="title">{title}</p>
-        <p className="location">{location}</p>
-        <p className="date">{date}</p>
-        <p className="time">{time}</p>
-        <p className="description">{description}</p>
-    </Link>
-  )
-}
+const EventCard = ({
+	title,
+	image,
+	slug,
+	location,
+	date,
+	time,
+	description,
+}: Props) => {
+	return (
+		<Link href={`/events/${slug}`} id="event-card">
+			<Image
+				src={image}
+				alt={title}
+				width={410}
+				height={300}
+				className="poster"
+			/>
 
-export default EventCard
+			<div className="flex flex-row gap-2">
+				<Image
+					src="/icons/pin.svg"
+					alt="location"
+					width={20}
+					height={20}
+				/>
+				<p>{location}</p>
+			</div>
+
+			<p className="title">{title}</p>
+
+			<div className="datetime">
+				<div>
+					<Image
+						src="/icons/calendar.svg"
+						alt="date"
+						width={20}
+						height={20}
+					/>
+					<p>{date}</p>
+				</div>
+				<div>
+					<Image
+						src="/icons/clock.svg"
+						alt="time"
+						width={20}
+						height={20}
+					/>
+					<p>{time}</p>
+				</div>
+			</div>
+		</Link>
+	);
+};
+
+export default EventCard;
